4197c553be1046f8dc2924c01cc26928274a91fa
matt-benton <mattbenton@hey.com>
2021-05-28 07:36:08 -0500

    Remove text from skills section

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,47 +1,34 @@
 <template>
   <section id="skills">
     <div class="section-container">
-      <div class="skills-container">
-        <div class="skills-left">
-          <p class="left-text">
-            I am a <b>JavaScript</b> and <b>PHP</b> developer with experience using various
-            frameworks and libraries. I really love using <b>Vue.js</b> and <b>Laravel</b> for my
-            projects. I enjoy writing plain <b>CSS</b>, but I have also used CSS libraries such as
-            <b>Bootstrap</b> and <b>TailwindCSS</b>. For end to end testing I prefer to use
-            <b>Cypress</b>.
-          </p>
+      <div class="skills-grid">
+        <div class="skills-grid-item">
+          <img src="/img/logos/html-5-logo-png-transparent-min.png" alt="html5 logo" />
+          html
         </div>
-        <div class="skills-right">
-          <div class="skills-grid">
-            <div class="skills-grid-item">
-              <img src="/img/logos/html-5-logo-png-transparent-min.png" alt="html5 logo" />
-              html
-            </div>
-            <div class="skills-grid-item">
-              <img src="/img/logos/css-3-logo-png-transparent-min.png" alt="css3 logo" />
-              css
-            </div>
-            <div class="skills-grid-item">
-              <img src="/img/logos/javascript-logo-png-transparent-min.png" alt="javascript logo" />
-              javascript
-            </div>
-            <div class="skills-grid-item">
-              <img src="/img/logos/vue-9-logo-png-transparent-min.png" alt="vue logo" />
-              vue
-            </div>
-            <div class="skills-grid-item">
-              <img src="/img/logos/php-1-logo-png-transparent-min.png" alt="php logo" />
-              php
-            </div>
-            <div class="skills-grid-item">
-              <img src="/img/logos/mysql-5-logo-png-transparent-min.png" alt="mysql logo" />
-              mysql
-            </div>
-            <div class="skills-grid-item">
-              <img src="/img/logos/laravel-1-logo-png-transparent-min.png" alt="laravel logo" />
-              laravel
-            </div>
-          </div>
+        <div class="skills-grid-item">
+          <img src="/img/logos/css-3-logo-png-transparent-min.png" alt="css3 logo" />
+          css
+        </div>
+        <div class="skills-grid-item">
+          <img src="/img/logos/javascript-logo-png-transparent-min.png" alt="javascript logo" />
+          javascript
+        </div>
+        <div class="skills-grid-item">
+          <img src="/img/logos/vue-9-logo-png-transparent-min.png" alt="vue logo" />
+          vue
+        </div>
+        <div class="skills-grid-item">
+          <img src="/img/logos/php-1-logo-png-transparent-min.png" alt="php logo" />
+          php
+        </div>
+        <div class="skills-grid-item">
+          <img src="/img/logos/mysql-5-logo-png-transparent-min.png" alt="mysql logo" />
+          mysql
+        </div>
+        <div class="skills-grid-item">
+          <img src="/img/logos/laravel-1-logo-png-transparent-min.png" alt="laravel logo" />
+          laravel
         </div>
       </div>
     </div>
@@ -60,36 +47,8 @@ section {
   line-height: var(--line-height-medium);
 }
 
-.skills-container {
-  display: flex;
-  padding: var(--sp-7);
-  margin: var(--sp-7) 0;
-}
-
-.skills-container > div {
-  width: 50%;
-}
-
-.skills-left {
-  display: flex;
-  align-items: center;
-  padding: var(--sp-6);
-}
-
-.left-text {
-  color: var(--color-grey-light-1);
-  font-weight: 300;
-}
-
-.skills-right {
-  display: flex;
-  align-items: center;
-  padding: var(--sp-6);
-}
-
 .skills-grid {
   display: flex;
-  width: 100%;
   flex-wrap: wrap;
 }
 
@@ -117,27 +76,5 @@ img {
   max-width: var(--text-2xl);
   margin-right: var(--sp-3);
   border: none;
-}
-
-@media (max-width: 768px) {
-  .skills-container > div {
-    padding: var(--sp-2);
-  }
-}
-
-@media (max-width: 640px) {
-  .skills-container {
-    flex-direction: column;
-    margin: 0;
-    padding: 0;
-  }
-
-  .skills-container > div {
-    width: 100%;
-  }
-
-  section {
-    padding: var(--sp-8) 5%;
-  }
 }
 </style>
