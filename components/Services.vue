6b9ee76ad32b0134728c8f2f25e3e8745ec5c12e
matt-benton <mattbenton@hey.com>
2022-05-05 10:27:22 -0500

    Update services

--- components/Services.vue
+++ components/Services.vue
@@ -47,15 +47,16 @@
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
+          stroke-width="2"
         >
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
           />
         </svg>
       </service-card>
@@ -92,9 +93,9 @@ export default {
         {
           title: 'Web application development',
           listItems: [
-            'Bring your idea to the web',
-            'Custom made SaaS applications',
-            'Software that solves unique problems',
+            'Laravel development with PHP best practices',
+            'Vue powered frontends',
+            'Test driven development',
           ],
         },
         {
@@ -106,12 +107,16 @@ export default {
           ],
         },
         {
-          title: 'E-commerce',
-          listItems: ['Put your store on the web', 'Sell products online', 'Collect payments'],
+          title: 'Database',
+          listItems: ['Design with data integrity', 'Fast queries and optimization'],
         },
         {
-          title: 'HTML / CSS',
-          listItems: ['Convert a design to html & css', 'Mobile-friendly', 'Accessible'],
+          title: 'HTML & CSS',
+          listItems: [
+            'Design conversion to HTML and CSS',
+            'Responsive and mobile-friendly',
+            'Accessible',
+          ],
         },
       ],
     }
